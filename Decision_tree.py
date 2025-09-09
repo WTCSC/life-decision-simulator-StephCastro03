73ee62980b1a32c1641fcac90f8ee598a010248b
Stephanie Castro <stephsnuggs1@gmail.com>
2025-09-09 14:45:42 -0600

    Incomplete Life Decisions

---
+++ Decision_tree.py
@@ -0,0 +1,14 @@
+print(" Welcome to the going to practice destiny simulator!")
+print(" This isn't just practice! This is a crossroad of desity!")
+print(" This could either lead to the teams glory or the collapes of civilization itself...")
+
+q1 = input(" Your alarm shrieks. Do you'get up' or 'sleep in'?").lower()
+
+if q1 == "get up":
+    q2 = input(" You stumble to your practice bag. Do you pack your 'shoes', 'water bottle', or 'forget something'?").lower()
+    if q2 == "shoes":
+        q3 = input(" You notice your shoes have a glow to them. Do you 'wear them' or 'be scared of them'").lower()
+        if q3 == "wear them":
+            q4 = input(" The shoes whisper: 'Go through the woods... it's faster' Do you listen? 'yes or 'no'?").lower()
+            if q4 == "yes":
+                q5 = input(" The raccon come out of their trees in the woods and challenge you to a fight. Do you use 'karate' or use a 'bride'?").lower()
